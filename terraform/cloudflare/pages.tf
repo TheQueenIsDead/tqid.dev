7237a883bddbacba015d668a7e0cbeabe491e329
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2026-07-08 08:26:27 +1200

    chore: wip import v4 pages resource instead of v5

--- terraform/cloudflare/pages.tf
+++ terraform/cloudflare/pages.tf
@@ -1,46 +1,34 @@
 resource "cloudflare_pages_project" "tqid-dev" {
-  account_id = var.cloudflare_account_id
-  name = "tqid-dev"
+  account_id        = var.cloudflare_account_id
+  name              = "tqid-dev"
   production_branch = "main"
-  build_config = {
-    build_caching = true
-    build_command = "npm run build"
-    destination_dir = "www"
-    root_dir = "www"
-  }
-  deployment_configs = {
-    production = {
-      always_use_latest_compatibility_date = false
-      build_image_major_version = 3
-      compatibility_date = "2025-01-01"
-      compatibility_flags = ["url_standard"]
-      fail_open = true
-      limits = {
-        cpu_ms = 100
-      }
-      placement = {
-        mode = "smart"
-      }
-      usage_model = "standard"
-    }
-  }
-  source = {
-    config = {
-      deployments_enabled = true
-      owner = "TheQueenIsDead"
-      owner_id = "12345678"
-      path_excludes = ["string"]
-      path_includes = ["string"]
-      pr_comments_enabled = true
-      preview_branch_excludes = ["string"]
-      preview_branch_includes = ["string"]
-      preview_deployment_setting = "all"
-      production_branch = "main"
-      production_deployments_enabled = true
-      repo_id = "12345678"
-      repo_name = "my-repo"
-    }
+
+  source {
     type = "github"
+    config {
+      owner                         = "TheQueenIsDead"
+      repo_name                     = "tqid.dev"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "custom"
+      preview_branch_includes       = ["dev", "preview"]
+      preview_branch_excludes       = ["main", "prod"]
+    }
+  }
+
+  build_config {
+    build_command       = "exit 0"
+    destination_dir     = "www"
+    root_dir            = ""
+  }
+
+  deployment_configs {
+    production {
+      compatibility_date  = "2022-08-16"
+      compatibility_flags = ["nodejs_compat", "streams_enable_constructors"]
+    }
   }
 }
 
